21fb54a220267b9b25aee48df83c1d06d1f68fbf
Aditya Dawadikar <adityadawadikar2000@gmail.com>
2020-09-05 20:59:52 +0530

    files rearranged,JWT fixed, Email template added

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const indexRoutes = require('../controllers/index');
+const emailTest = require('../modules/smtp');
 
 router.get('/', indexRoutes.getIndexPage);
+
+email = "adityadawadikar2000@gmail.com";
+
+router.get("/email", async(req, res) => {
+    emailTest.successful_registration(email);
+    res.status(200);
+    res.end();
+});
 
 module.exports = router;
