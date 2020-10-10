8280cf1a75c9185f1430fe5ebaa8d648988392e0
IzacVrushabh <vrushabhkulye@gmail.com>
2020-10-10 23:39:59 +0530

    post menuList

--- client/src/components/dashboard/mess-dash/MySavedMenu.jsx
+++ client/src/components/dashboard/mess-dash/MySavedMenu.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MessSettings.css";
-import EditIcon from '@material-ui/icons/Edit';
+import EditIcon from "@material-ui/icons/Edit";
+import StarsIcon from '@material-ui/icons/Stars';
+import { authAxiosMess } from "../../../App";
 
 const MySavedMenu = () => {
+  // const getId = localStorage.getItem("userIdMess");
+  // const [menu , setMenu] = useState([]);
+
+  // useEffect(()=>{
+  //   authAxiosMess
+  //   .get(`api/menu/all/${getId}`)
+  //   .then((res) => {
+  //     console.log(res);
+  //     console.log(res.data);
+  //     setMenu(res.data);
+  //   })
+  //   .catch((err) => console.log(`${err} :some error while fetching menu list`));
+  // },[])
+
   return (
     <div className="mt-4" style={{ width: "70%" }}>
       <div className="edit-profile ml-1 container">
         <div style={{ backgroundColor: "#FFF8DE" }}>
-          <ul className="col sm-12 mess-menus">
+          <ul
+            className="col sm-12 d-flex justify-content-between align-items-center bg-white"
+            style={{ borderRadius: "5px", height: "75px", borderLeft:"8px solid #FFB800" }}
+          >
             <li>
               Aloo Sabji, Roti, Dal Rice, Papad, Pickle, Taak
               <img
@@ -15,9 +34,15 @@ const MySavedMenu = () => {
                 alt="veg"
               />
             </li>
-            <li style={{ color: "#FF5C00" }}>60 INR</li>
-            <li><EditIcon style={{color:"#FFB800"}}/></li>
+            <li className="font-weight-bold" style={{ color: "#FF5C00",letterSpacing:"2px" }}>60 INR</li>
             <li>Veg</li>
+            <li>
+              <StarsIcon style={{ color: "#FFB800" }} />
+            </li>
+            <li className="mr-3">
+              <EditIcon style={{ color: "#FFB800" }} />
+            </li>
+            
           </ul>
           <ul className="col my-2 sm-12 mess-menus">
             <li>
@@ -28,7 +53,9 @@ const MySavedMenu = () => {
               />
             </li>
             <li style={{ color: "#FF5C00" }}>40 INR</li>
-            <li><EditIcon style={{color:"#FFB800"}}/></li>
+            <li>
+              <EditIcon style={{ color: "#FFB800" }} />
+            </li>
             <li>Veg</li>
           </ul>
           <ul className="col sm-12 mess-menus">
@@ -40,7 +67,9 @@ const MySavedMenu = () => {
               />
             </li>
             <li style={{ color: "#FF5C00" }}>75 INR</li>
-            <li><EditIcon style={{color:"#FFB800"}}/></li>
+            <li>
+              <EditIcon style={{ color: "#FFB800" }} />
+            </li>
             <li>Veg</li>
           </ul>
           <ul className="col sm-12 mess-menus">
@@ -52,7 +81,9 @@ const MySavedMenu = () => {
               />
             </li>
             <li style={{ color: "#FF5C00" }}>90 INR</li>
-            <li><EditIcon style={{color:"#FFB800"}}/></li>
+            <li>
+              <EditIcon style={{ color: "#FFB800" }} />
+            </li>
             <li>Veg</li>
           </ul>
           <ul className="col sm-12 mess-menus">
@@ -64,7 +95,9 @@ const MySavedMenu = () => {
               />
             </li>
             <li style={{ color: "#FF5C00" }}>35 INR</li>
-            <li><EditIcon style={{color:"#FFB800"}}/></li>
+            <li>
+              <EditIcon style={{ color: "#FFB800" }} />
+            </li>
             <li>Veg</li>
           </ul>
         </div>
