bd7214a2b4aa9bf479cfd39fa5dcfd83ec615f39
Neha121s <nehashelar121@gmail.com>
2020-10-12 14:00:02 +0530

    Get Reviews API

--- server/app/controllers/modules/review.js
+++ server/app/controllers/modules/review.js
@@ -2,18 +2,104 @@ const Mess = require('../../models/mess');
 const Customer = require('../../models/customer');
 const Reviews = require('../../models/relations/reviews');
 
-//helper function
-function updateOverAllrating(newRating, messId) {
 
+//helper function
+function calculateOverAllrating(newRating) {
+    return newRating;
 }
 
-exports.getAllReviews = (req, res) => {
-
+exports.getAllReviews = async(req, res) => {
+await Mess.findById({_id:req.params.messId})
+    .select('Reviews')
+    .then(doc=>{
+        res.status(200).json({
+            message:"success",
+            doc:doc
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: "some error occured while fetching data",
+            error: "internal server error"
+        });
+    });
 }
 
 //controllers to handle rating
-exports.addReview = (req, res) => {
+exports.addReview = async(req, res) => {
+    let comment = {
+        comment: req.body.comment,
+        author: req.params.custId,
+        timestamp: req.body.timestamp
+    } 
+    let thread = []
+    thread.push(comment)
+    const review=new Reviews({
+        messId:req.params.messId,
+        customerId:req.params.custId,
+        rating: req.body.rating,
+        thread: thread
+    })
 
+    let reviewId=0
+
+    await review.save()
+    .then(doc => {
+        reviewId=doc._id
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: "some error occured while storing new review",
+            error: "internal server error"
+        });
+    });
+
+    let reviewArray=[]
+    let sum = 0
+
+    await Mess.findById({_id:req.params.messId})
+    .then(doc=>{
+        reviewArray=doc.Reviews.reviewers
+        sum=doc.Reviews.sum
+        console.log(sum)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: "some error occured while updating document",
+            error: "internal server error"
+        });
+    });
+
+    reviewArray.push({
+        reviewId:reviewId
+    })
+
+    
+    sum+=req.body.rating
+    let updatedRating=Number(sum)
+
+    let reviewObject={
+        sum: sum,
+        reviewers: reviewArray
+    }
+    console.log(reviewObject)
+
+    await Mess.findByIdAndUpdate({_id:req.params.messId},{Reviews:reviewObject}) //missing Rating:updatedRating
+    .then(doc=>{
+        res.status(200).json({
+            message:"successfully rated"
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: "some error occured while updating document",
+            error: "internal server error"
+        });
+    });
 }
 
 exports.updateRating = (req, res) => {
